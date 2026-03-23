ef43990e4a837cfd0d3ab5cf13a15b4d229ffae5
majddiab33-design <majd.diab33@gmail.com>
2026-03-23 12:26:27 +0200

    t

--- src/app/components/AttractionComp/AttractionsCard.js
+++ src/app/components/AttractionComp/AttractionsCard.js
@@ -1,26 +1,30 @@
 'use client'
 import styles from "./AttractionsCard.module.css";
 
-export default function AttractionsCard({id, name, rate, image }) {
+export default function AttractionsCard({ id, name, rate, image }) {
     return (
         <div className={styles.cardsContainer}>
-            
-                <div key={id} className={styles.card}>
-                    <img
-                        src={image}
-                        alt={name}
-                        className={styles.cardImage}
-                    />
 
-                    <div className={styles.cardBody}>
-                        <h3 className={styles.cardName}>{name}</h3>
+            <div key={id} className={styles.card}>
+                <img
+                    src={image}
+                    onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = "https://placehold.co/400";
+                    }}
+                    alt={name}
+                    className={styles.cardImage}
+                />
 
-                        <div className={styles.cardRating}>
-                            ⭐ {rate}
-                        </div>
+                <div className={styles.cardBody}>
+                    <h3 className={styles.cardName}>{name}</h3>
+
+                    <div className={styles.cardRating}>
+                        ⭐ {rate}
                     </div>
                 </div>
-        
+            </div>
+
         </div>
     );
 }
